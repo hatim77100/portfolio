fa236e624e574a761c62e288868a2cb8beee62b5
hatim77100 <najoua-hatim@hotmail.fr>
2022-10-27 11:41:37 +0200

    update

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Alkalami&family=Open+Sans:wght@300&family=Poppins:ital,wght@1,100&display=swap"
           rel="stylesheet"
