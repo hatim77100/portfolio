c15a79b51d3ec2d903815d2199ce528111ac7ebd
hatim77100 <najoua-hatim@hotmail.fr>
2022-11-05 13:41:04 +0100

    update project button

--- src/components/homepage/1-firstSection/Button.jsx
+++ src/components/homepage/1-firstSection/Button.jsx
@@ -1,6 +1,6 @@
  const Button = () => {
     return (
-        <button>projets</button>
+        <button className={{style:"cursor-pointer"}}> <a href="#creations" >projets</a></button>
     )
 }
 export default Button;
